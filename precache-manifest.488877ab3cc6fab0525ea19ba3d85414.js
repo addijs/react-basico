8ab7d94e13a48c44d0b7d816df4b2fb6489b9c00
Adilson Junior <asc.juninho@hotmail.com>
2019-10-04 11:35:31 -0300

    Updates

---
+++ precache-manifest.488877ab3cc6fab0525ea19ba3d85414.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0302f55512aea367294076cfa01a3f7e",
+    "url": "/react-basico/index.html"
+  },
+  {
+    "revision": "be85c67cdc99ef2d6aaa",
+    "url": "/react-basico/static/css/main.837b7ee1.chunk.css"
+  },
+  {
+    "revision": "b2cfff019658565185e1",
+    "url": "/react-basico/static/js/2.1300750c.chunk.js"
+  },
+  {
+    "revision": "be85c67cdc99ef2d6aaa",
+    "url": "/react-basico/static/js/main.96a16709.chunk.js"
+  },
+  {
+    "revision": "22b2c1a2973334e133bf",
+    "url": "/react-basico/static/js/runtime-main.9cd27b51.js"
+  },
+  {
+    "revision": "99fa9647a7026749d4ed29fb2b98c3ec",
+    "url": "/react-basico/static/media/banner1.99fa9647.jpg"
+  }
+]);
